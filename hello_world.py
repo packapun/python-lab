133dd032bc9d61df4f69e55c9426f42babda4f8c
packapun <packapun.dev@gmail.com>
2025-06-26 22:39:00 -0400

    Implement selection sort

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,24 @@
 print('Hello world')
 
+def selectionSort(arr):
+    # Find the smallest element and move it to the end of the sorted boundary 
+    n = len(arr) 
+    
+    # Check for the base case 
+    if n < 2:
+        return arr
+    
+    for i in range(n-1):
+        minIndex = i 
+
+        for j in range(i+1,n):
+            if arr[j] < arr[minIndex]:
+                minIndex = j 
+        
+        if minIndex != i:
+            arr[i],arr[minIndex] = arr[minIndex],arr[i]
+
+
 
 def insertionSort(a): 
     n = len(a)
@@ -15,7 +34,6 @@ def insertionSort(a):
             k -= 1
         
         a[k + 1] = curr
-    
     return a
 
 # O(n^2)
@@ -28,5 +46,6 @@ def bubble_sort(a):
                 a[j],a[j+1] = a[j+1],a[j]
         print(a)
 
-a = [6,5,4,3,2,1]
-insertionSort(a)
+a = [1,6,3,8,2,10,0]
+selectionSort(a)
+print(a)
