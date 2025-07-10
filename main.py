88d1e1993ffa902395a253c54e6774ea3a25b56f
packapun <packapun.dev@gmail.com>
2025-07-10 13:03:07 -0400

    Update main

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-from typing import List
-# TwoSum problem 
-def twoSum(nums:List[int], target:int) -> List[int]:
+from typing import List 
+
+def mergesort(nums:List[int]):
     if not nums:
-        return []   
-    sorted_nums = sorted(nums)
-    left = 0
-    right = len(nums)-1
-    while left < right:
-        current_sum = nums[left] + nums[right]
-        if current_sum < target:
-            left += 1
-        elif current_sum > target:
-            right -= 1
+        return 
+    l = 0 
+    r = len(nums)-1
+    mergesort_util(nums,l,r)
+    
+    
+def mergesort_util(nums,l,r):
+    if l < r:
+        mid = (l+r)//2
+        mergesort_util(nums,l,mid)
+        mergesort_util(nums,mid+1,r)
+        merge(nums,l,r,mid)
+        
+
+
+def merge(arr,l,r,mid):
+    left = arr[l:mid+1]
+    right = arr[mid+1:r+1]
+    i = j = 0
+    k = l 
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
         else:
-            return [left,right]
-    return []
+            arr[k] = right[j]
+            j += 1
+        k += 1
 
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
 
+def test_mergesort():
+    nums = [5,4,3,2,1]
+    mergesort(nums)
+    print(nums)
 
-
-# Test case
-def test_twosum():
-    nums = [5,2,1,7]
-    target = 12
-    assert twoSum(nums,target) == [0,3]
-
-test_twosum()
-
+test_mergesort()
