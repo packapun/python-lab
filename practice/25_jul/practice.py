50a742ef14dd497329722a1c511cfb2c2e641c9d
packapun <packapun.dev@gmail.com>
2025-07-26 15:18:39 -0400

    Solve set zeroes problem

--- practice/25_jul/practice.py
+++ practice/25_jul/practice.py
@@ -324,3 +324,43 @@ test_insert_interval()
 
 
 # TTS (time to solve) : 30min
+
+
+# =============
+def setZeroes(self, matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    rows = len(matrix)
+    cols = len(matrix[0])
+    first_row_zero = False
+    first_col_zero = False
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0: # [1,1]
+                if i == 0:
+                    first_row_zero = True
+                if j == 0:
+                    first_col_zero = True
+                matrix[i][0] = 0 # [1,0]
+                matrix[0][j] = 0 # [0,1]
+    
+    for j in range(1,cols):
+        if matrix[0][j] == 0:
+            for i in range(rows):
+                matrix[i][j] = 0
+
+    for i in range(1,rows):
+        if matrix[i][0] == 0:
+            for j in range(cols):
+                matrix[i][j] = 0
+
+    if first_row_zero:
+        for j in range(cols):
+            matrix[0][j] = 0
+
+    if first_col_zero:
+        for i in range(rows):
+            matrix[i][0] = 0
+
+# TTS (time to solve) : 45min
