5360ab3be3ff272394e6a85cdcfd3341846f75d9
packapun <packapun.dev@gmail.com>
2025-07-26 16:40:46 -0400

    Max consecutive ones

--- main.py
+++ main.py
@@ -96,3 +96,50 @@ test_insert_interval()
 
 
 # TTS (time to solve) : 30min
+
+
+# ===========
+def lengthOfLongestSubstring(self, s: str) -> int:
+    max_length = 0
+    left = right = 0
+    seen = {}
+    while right < len(s):
+        if s[right] not in seen:
+            # Unseen character
+            seen[s[right]] = right  # a:0
+        else:
+            # We've seen this character 
+            prev_index = seen[s[right]] # 0
+            if prev_index >= left: 
+                left = prev_index + 1
+            seen[s[right]] = right 
+        
+        max_length = max(max_length, right - left + 1)
+        right += 1
+    return max_length 
+
+# TTS : 20min
+
+
+# ===============
+
+def longestOnes(self, nums: List[int], k: int) -> int:
+    zero_count = 0
+    left = right = 0
+    max_count = 0 
+    
+    while right < len(nums):
+        if nums[right] == 0:
+            zero_count += 1 #3
+            while zero_count > k:
+                if nums[left] != 0:
+                    left += 1
+                else:
+                    zero_count -= 1
+                    left += 1
+
+        max_count = max(max_count, right - left + 1) #1
+        right += 1
+    return max_count
+
+# TTS : 10:42min
