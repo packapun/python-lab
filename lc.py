7bb73b60683a2edc465004eb46ed17132169ca90
packapun <packapun.dev@gmail.com>
2025-08-02 13:29:28 -0400

    Update lc.py
    3 sum

--- lc.py
+++ lc.py
@@ -369,7 +369,39 @@ def gcdOfStrings(self, str1: str, str2: str) -> str:
         return candidate
     
     return ""
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    if not nums or len(nums) < 3 :
+        return []
+    n = len(nums)
+    nums.sort()
+    output = []
+    print(nums)
+    for i in range(n-2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        if nums[i] > 0:
+            break 
         
+        left = i+1
+        right = n-1
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            if current_sum == 0:
+                output.append([nums[i], nums[left], nums[right]])
+                
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+                while left < right and nums[right] == nums[right-1]:
+                    right -= 1
+                left += 1
+                right -= 1
+            elif current_sum > 0:
+                right -= 1
+            else:
+                left += 1
+    return output
 
 
 
